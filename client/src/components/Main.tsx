adb173e098e2ac62615c7d57b276fbce882545a9
Shestoy06 <pochta5550@gmail.com>
2025-08-21 02:56:52 +0200

    💄 style: App - Add credits

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -7,6 +7,7 @@ import VoiceChatIcon from '@/assets/voice-chat.svg'
 import Tappable from '@/components/ui/Tappable'
 import Button from '@/components/ui/Button'
 import NextIcon from '@/assets/next.svg'
+import GithubIcon from '@/assets/github.svg'
 
 const Main = () => {
 	const global = getGlobal()
@@ -31,64 +32,75 @@ const Main = () => {
 	}
 
 	return (
-		<div>
-			<div class="flex flex-col h-screen">
-				<div class="flex-1 flex flex-col items-center justify-between">
-					<div class="py-4">
-						<SettingsButton />
-					</div>
+		<div class="flex flex-col h-screen relative">
+			<div class="flex-1 flex flex-col items-center justify-between relative">
+				<div class="py-4">
+					<SettingsButton />
 				</div>
-				<div class="flex-1 flex flex-col items-center justify-center">
-					<Switch>
-						<Match when={!ownState().result && !ownState().isLoading}>
-							<div class="flex flex-col items-center justify-center">
-								<div class="text-2xl">Accentless</div>
-								<div class="mt-2">Improve your pronunciation with AI </div>
+				<div
+					class="absolute right-2 top-2 cursor-pointer"
+					onClick={() =>
+						window.open(
+							'https://github.com/shestaya-liniya/ai-pronunciation-trainer-better-ui',
+						)
+					}
+				>
+					<GithubIcon class="h-10 w-10" />
+				</div>
+			</div>
+			<div class="flex-1 flex flex-col items-center justify-center">
+				<Switch>
+					<Match when={!ownState().result && !ownState().isLoading}>
+						<div class="flex flex-col items-center justify-center">
+							<div class="text-2xl">Accentless</div>
+							<div class="mt-2">Improve your pronunciation with AI </div>
+						</div>
+					</Match>
+					<Match when={ownState().result}>
+						<div class="px-4 flex flex-col items-center justify-center animate-fadeIn-blurOut">
+							<div class="text-center text-2xl select-text">
+								{ownState().result?.text}
 							</div>
-						</Match>
-						<Match when={ownState().result}>
-							<div class="px-4 flex flex-col items-center justify-center animate-fadeIn-blurOut">
-								<div class="text-center text-2xl select-text">
-									{ownState().result?.text}
+							<div class="text-center text-white/50 font-mono mt-2">
+								{ownState().result?.ipa}
+							</div>
+							<div class="grid grid-cols-[1fr_auto_1fr] w-80 relative top-8">
+								<div class="flex justify-start">
+									<Button
+										variant="ghost"
+										class="text-white/50 text-sm gap-0"
+										onClick={() => {}}
+									>
+										Score 0
+									</Button>
 								</div>
-								<div class="text-center text-white/50 font-mono mt-2">
-									{ownState().result?.ipa}
-								</div>
-								<div class="grid grid-cols-[1fr_auto_1fr] w-80 relative top-8">
-									<div class="flex justify-start">
-										<Button
-											variant="ghost"
-											class="text-white/50 text-sm gap-0"
-											onClick={() => {}}
-										>
-											Score 0
-										</Button>
-									</div>
-									<Tappable class="rounded-full p-2" onClick={() => {}}>
-										<VoiceChatIcon class="h-6 w-6 relative left-[1px] top-[1px]" />
-									</Tappable>
-									<div class="flex justify-end">
-										<Button
-											variant="ghost"
-											class="text-white/50 text-sm gap-0"
-											onClick={handleSkip}
-										>
-											Skip
-											<NextIcon class="h-4 w-4" />
-										</Button>
-									</div>
+								<Tappable class="rounded-full p-2" onClick={() => {}}>
+									<VoiceChatIcon class="h-6 w-6 relative left-[1px] top-[1px]" />
+								</Tappable>
+								<div class="flex justify-end">
+									<Button
+										variant="ghost"
+										class="text-white/50 text-sm gap-0"
+										onClick={handleSkip}
+									>
+										Skip
+										<NextIcon class="h-4 w-4" />
+									</Button>
 								</div>
 							</div>
-						</Match>
-					</Switch>
-				</div>
-				<div class="flex-1 flex items-center justify-center py-12">
-					<VoiceVisualizer
-						isRecording={isRecording()}
-						toggleIsRecording={handleToggleRecording}
-						hint={hint()}
-					/>
-				</div>
+						</div>
+					</Match>
+				</Switch>
+			</div>
+			<div class="flex-1 flex items-center justify-center py-12">
+				<VoiceVisualizer
+					isRecording={isRecording()}
+					toggleIsRecording={handleToggleRecording}
+					hint={hint()}
+				/>
+			</div>
+			<div class="absolute bottom-4 right-4 text-xs text-white/50">
+				By Thiago Lobato and Andrei Silin.
 			</div>
 		</div>
 	)
