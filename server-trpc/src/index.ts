a5955f62086327a0ed0bd63cc059943007e09af0
shestaya_liniya <pochta5550@gmail.com>
2025-08-25 20:58:58 +0200

    🔧 config: Server - Setup cors

--- server-trpc/src/index.ts
+++ server-trpc/src/index.ts
@@ -1,15 +1,26 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 import { WorkerEntrypoint } from 'cloudflare:workers'
 
+import { addCORSHeaders, handleCORSPreflight } from './cors'
 import { appRouter } from './router'
 
-export default class TRPCCloudflareWorkerExample extends WorkerEntrypoint {
+export interface Env {
+	CLIENT_ORIGIN: string
+}
+
+export default class TRPCCloudflareWorkerExample extends WorkerEntrypoint<Env> {
 	async fetch(request: Request): Promise<Response> {
-		return fetchRequestHandler({
+		if (request.method === 'OPTIONS') {
+			return handleCORSPreflight(request, this.env)
+		}
+
+		const response = await fetchRequestHandler({
 			endpoint: '/trpc',
 			req: request,
 			router: appRouter,
-			createContext: () => ({}),
+			createContext: () => ({ env: this.env }),
 		})
+
+		return addCORSHeaders(request, response, this.env)
 	}
 }
